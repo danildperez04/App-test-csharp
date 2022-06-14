d8b914d15d4c9843def6899e51154e9e25152af2
danildperez04 <gamerdan589@gmail.com>
2022-06-14 08:03:54 -0600

    Tabbed Pane

--- App-test-csharp/obj/Debug/net6.0-windows/App.g.i.cs
+++ App-test-csharp/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace App_test_csharp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace App_test_csharp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public static void Main() {
             App_test_csharp.App app = new App_test_csharp.App();
             app.InitializeComponent();
