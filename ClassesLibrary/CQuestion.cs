3d43850eefb5a111988a8bd3012111fad149fd71
danildperez04 <gamerdan589@gmail.com>
2022-06-26 12:50:36 -0600

    CQuestion & CResults

--- ClassesLibrary/CQuestion.cs
+++ ClassesLibrary/CQuestion.cs
@@ -1,12 +1,88 @@
-﻿using System;
+﻿using App_test_csharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ClassesLibrary
 {
     public class CQuestion
     {
+
+        public void pressButton(string textBtn)
+        {
+            if (n < questions.Count)
+            {
+                answerIsCorrect(textBtn);
+                updateQuestion(questions[sequenceQuestions[n]]);
+                n++;
+            }
+            else
+            {
+                FrmResultados frmResults = new FrmResultados();
+                CResults cResults = new CResults(frmResults, questions);
+                frmResults.Show();
+                view.Close();
+            }
+        }
+
+        public void answerIsCorrect(string textBtn)
+        {
+            Question question = questions[sequenceQuestions[n - 1]];
+            Console.WriteLine(textBtn + " == " + question.CorrectAnswer);
+            if (textBtn.Equals(question.CorrectAnswer))
+            {
+                question.IsCorrect = true;
+            }
+        }
+
+        public void updateQuestion(Question nextQuestion)
+        {
+            Console.WriteLine(nextQuestion);
+            int i = 0;
+            foreach (string answers in nextQuestion.Answers)
+            {//Recorre todas las preguntas
+                btnAnswers[i].Content = answers;
+                i++;
+            }
+            /*view.LblPregunta.Text = nextQuestion.QuestionLabel;
+            view.LblImagenPregunta.Icon = nextQuestion.ImageLabel;*/
+            Console.WriteLine("Question updated\n\n");
+        }
+
+        public void getSequence(int max)
+        {
+            //Generate a random sequence for questions in test
+            Random rnd = new Random();
+            for (int i = 0; i < max; i++)
+            {
+                int aleatorio = -1;
+                bool generado = false;
+
+                while (!generado)
+                {
+                    int number = rnd.Next(max);
+                    if (!sortedQuestions.Contains(number))
+                    {
+                        sortedQuestions.Add(number);
+                        aleatorio = number;
+                        generado = true;
+                    }
+                }
+                sequenceQuestions.Add(aleatorio);
+            }
+            Console.WriteLine(sequenceQuestions.ToString());
+        }
+
+        private FrmQuestions view;
+        private Question model;
+        private Test testModel = new Test();
+        private Button[] btnAnswers = new Button[4];
+        private List<Question> questions = null;
+        private HashSet<int> sortedQuestions = new HashSet<int>();
+        private List<int> sequenceQuestions = new List<int>();
+        private int n = 1;
     }
 }
--- ClassesLibrary/CResults.cs
+++ ClassesLibrary/CResults.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using App_test_csharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ClassesLibrary
 {
     public class CResults
     {
+        public CResults() { }
+        public CResults(FrmResultados view, List<Question> model)
+        {
+            this.view = view;
+            this.questions = model;
+            /*this.btnsHelp = new Button[] { view.Button1, view.Button2, view.Button3, view.Button4, view.Button5, view.Button6, view.Button7, view.Button8, view.Button9, view.Button10 };
+            this.questionsLabel = new Label[] { view.Question1, view.Question2, view.Question3, view.Question4, view.Question5, view.Question6, view.Question7, view.Question8, view.Question9, view.Question10 };
+            initBtnsHelp();
+            initQuestionsLabel();
+            initScore();*/
+        }
+
+        private FrmResultados view;
+        private Button[] btnsHelp = new Button[10];
+        private Label[] questionsLabel = new Label[10];
+        private List<Question> questions = null;
+        private string score = null;
+        private int n = 1;
     }
 }
