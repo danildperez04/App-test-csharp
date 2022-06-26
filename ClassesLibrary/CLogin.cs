7d153b0ff1ac36dfb72a608edf35ff27fab53e47
danildperez04 <gamerdan589@gmail.com>
2022-06-26 11:55:27 -0600

    FileStream created

---
+++ ClassesLibrary/CLogin.cs
@@ -0,0 +1,16 @@
+﻿using App_test_csharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class CLogin
+    {
+        FrmLogin view;
+        User model;
+        private Dictionary<string, User> users = new HashMap<string, User>();
+    }
+}
---
+++ ClassesLibrary/FileStream.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class FileStream
+    {
+        public static Dictionary<string, User> GetUsers()
+        {
+            try
+            {
+                string json = File.ReadAllText(fileName);
+                users = JsonSerializer.Deserialize<Dictionary<string, User>>(json);
+
+                return users;
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("NO SE HA ENCONTRADO EL ARCHIVO");
+                return null;
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("ERROR DE EJECUCION");
+                return null;
+            }
+        }
+
+        public static void setNewUser(User user)
+        {
+            try
+            {
+                users.Add(user.Carnet, user);
+                string json = JsonSerializer.Serialize(users);
+                File.WriteAllText(fileName, json);
+
+                Console.WriteLine("SE CREO EL ARCHIVO");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Ha ocurrido un error! \nNo hemos encontrado la ruta del archivo especificado");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Ha ocurrido un error! \nLa ejecucion del programa ha sido interrumpida\n" + io.toString());
+            }
+        }
+
+
+        static string fileName = "users.out";
+        static Dictionary<string, User> users = new Dictionary<string, User>();
+    }
+}
